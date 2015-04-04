74151f8c25f6d9b6122f0f661f6bf12f4a3e149d
ShikhaMehta <smehta11@asu.edu>
2015-04-04 16:40:01 -0700

    initial php test

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ $OPENSHIFT_MYSQL_DB_PORT = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $dbname = "patientdb";
 
 // debug
-echo $OPENSHIFT_MYSQL_DB_HOST . "\n";
-echo $OPENSHIFT_MYSQL_DB_PORT . "\n";
+//echo $OPENSHIFT_MYSQL_DB_HOST . "\n";
+//echo $OPENSHIFT_MYSQL_DB_PORT . "\n";
 
 $link = mysql_connect("$OPENSHIFT_MYSQL_DB_HOST","adminCjp7HQy","nuL1MDKTyQkl") or die("Error: Cannot connect to database.\n"); 
 mysql_select_db($dbname);
@@ -21,14 +21,16 @@ $query = "SELECT * FROM authentication";
 $result = mysql_query($query); 
 
 //display information: 
+echo "<h2>Please Login</h2>";
 
 if ($result) {
     while($row = mysql_fetch_array($result)) {
         $username = $row["username"];
 		$password = $row["password"];
 		$DoctorOrPatient = $row["DoctorOrPatient"];
-        echo "<h2>some data</h2>";
         echo "$username<br>";
+		echo "$password<br>";
+		echo $DoctorOrPatient<br>";
     }
 } 
 ?> 
