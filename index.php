f262eec5aa8c4f05661f362125a992570e892b04
ShikhaMehta <smehta11@asu.edu>
2015-04-04 16:37:46 -0700

    initial php test

--- index.php
+++ index.php
@@ -5,32 +5,30 @@ $OPENSHIFT_MYSQL_DB_HOST = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $OPENSHIFT_MYSQL_DB_PORT = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $dbname = "patientdb";
 
+// debug
+echo $OPENSHIFT_MYSQL_DB_HOST . "\n";
+echo $OPENSHIFT_MYSQL_DB_PORT . "\n";
+
 $link = mysql_connect("$OPENSHIFT_MYSQL_DB_HOST","adminCjp7HQy","nuL1MDKTyQkl") or die("Error: Cannot connect to database.\n"); 
 mysql_select_db($dbname);
 
-//define the query
+//consultation: 
 
 $query = "SELECT * FROM authentication"; 
 
-//execute the query
+//execute the query. 
 
 $result = mysql_query($query); 
 
 //display information: 
-
-echo "<html><title>Engineers Without Borders</title>\n";
-echo "<body>"
-echo "<h2>Welcome</h2>Please login with your username and password.<p>";
 
 if ($result) {
     while($row = mysql_fetch_array($result)) {
         $username = $row["username"];
 		$password = $row["password"];
 		$DoctorOrPatient = $row["DoctorOrPatient"];
-        echo "$username<br>\n";
-		echo "$password<br>\n";
-		echo "$DoctorOrPatient<br>\n";
+        echo "<h2>some data</h2>";
+        echo "$username<br>";
     }
-}
-echo "</body></html>\n"; 
+} 
 ?> 
