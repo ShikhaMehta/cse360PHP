ec2bb16cc28cd6620a11bd637021ac181fa7fd40
ShikhaMehta <smehta11@asu.edu>
2015-04-04 16:17:49 -0700

    initial php test

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 //conection: 
+$OPENSHIFT_MYSQL_DB_HOST = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$OPENSHIFT_MYSQL_DB_PORT = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $link = mysqli_connect("mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/","adminCjp7HQy","nuL1MDKTyQkl","patientdb") or die("Error " . mysqli_error($link)); 
 
 //consultation: 
