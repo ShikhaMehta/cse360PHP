6e5cbc90c9395465d48d665ae226bf152582085b
Gene <gdodge@gmail.com>
2015-04-09 12:02:07 -0700

    Renamed index.html to old.index.html in order for index.php to replace it. Added Controller.php which contains Controller class from which the other controller classes can inherit. Added index.php which has the HTML page and controls the flow of which web interface script is called. Added project.css file from which the HTML pages inherit their CSS.

---
+++ Controller.php
@@ -0,0 +1,152 @@
+<?php
+
+/*------------------------------------------------
+// by: Gene Dodge
+//
+//    THIS CLASS IS NOT IMPLEMENTED DIRECTLY
+//
+//  This class serves as a template for those
+//    classes which inherit from it and it's
+//    attributes and methods are placeholders
+//    that are to be redefined by it's child
+//    classes
+//
+//  Extends:
+//    NONE
+//
+//  Extended by:
+//    LoginController
+//    PatientController
+//    DoctorController
+//
+//  Requires:
+//    NONE
+//
+//  Required by:
+//    NONE
+//
+//  Attributes:
+//    $currentUser          // used by extending classes for Patient or Doctor
+//    $databaseQueryString  // SQL query string for querying database
+//    $queryData            // holds database query results
+//
+//  Methods:
+//   __construct()  // used to set default variables
+//                  //  REQUIRES extending classes call parent::__construct();
+//                  //  inside it's own __construct() method
+//   parseWebData() // deprecated as PHP handles this directly with $_POST
+//   queryDatabase(String) // returns results of query to $queryData
+//   parseDatabaseReturnInfo() // defined in extending classes accordingly
+//
+------------------------------------------------*/
+
+// database setup for doing MySQL queries
+// This redefines constants that can be used to connect
+//   to the MySQL database on Openshift.com
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+
+// global variables that can be used for connecting
+// for a specific example of connection see the function
+//   queryDatabase()
+$dbhost = constant("DB_HOST");     // Host name 
+$dbport = constant("DB_PORT");     // Host port
+$dbusername = constant("DB_USER"); // Mysql username 
+$dbpassword = constant("DB_PASS"); // Mysql password 
+$db_name = constant("DB_NAME");    // Database name
+
+Class Controller {
+	protected $currentUser;
+	protected $databaseQueryString;
+	protected $queryData;
+	
+	//--------- construct -------------
+	// The constructor for Controller sets
+	//  the variables to null to eliminate errors
+	//!! REQUIRES extending classes call parent::__construct();
+	//    inside it's own __construct() function
+	private function __construct() {
+		$this->currentUser = NULL;
+		$this->databaseQueryString = "";
+		$this->queryData = NULL;
+	}
+	
+	// ---------------- setQueryString ------------------
+	// This function takes a string and sets the
+	//   $databaseQueryString equal to it.
+	protected function setQueryString($newQueryString) {
+		$this->databaseQueryString = $newQueryString;
+	}
+	
+	// ---------------- getQueryString ------------------
+	// Returns the current value of $databaseQueryString
+	protected function getQueryString() {
+		return $this->databaseQueryString;
+	}
+	
+	//-----------------  queryDatabase() ---------------------------
+	// This function uses $databaseQueryString (a formatted SQL
+	//  query string) and queries the MySQL database with it.
+	//
+	// Does not return results directly, but populates $queryData
+	//  with the results in the form of a mysqli_query
+	//
+	// Example:
+	//   If we had a table with the following format and data
+	//
+	//   science_peoples
+	//   ----------------------------------------------
+	//  | first_name  |  last_name   |  phone_number   |
+	//   ----------------------------------------------
+	//  |  Albert     |  Einstein    |  1112223333     |
+	//   ----------------------------------------------
+	//  |  Isaac      |  Newton      |  4445556666     |
+	//   ----------------------------------------------
+	//
+	//  and we called queryDatabase("SELECT * FROM science_peoples");
+	//  the results stored in $queryData could be accessed by:
+	//
+	//  if (mysqli_num_rows($queryData) > 0) {
+	//     while ($row = mysqli_fetch_assoc($queryData)) {
+	//         // do stuff with the data from this row
+	//         // the array would hold values for each row in
+    //         //  this format:	
+	//         // $row["first_name"] , $row["last_name"], $row["phone_number"]
+	//         // where the first row would correspond to:
+	//         // "Albert", "Einstein", 1112223333
+	//     }
+	//
+	protected function queryDatabase() {
+		// if the query string is not populated, issue warning
+		//  and do not query the database
+		if (empty($this->databaseQeuryString)) {
+			echo "!! No SQL query string defined. Please use setQueryString(<your_SQL_query_string>) prior to calling queryDatabase(). !!";
+		} else {
+			// open a connection to the database, failing if there is an error
+			$mysqlConnection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, "", DB_PORT) or die("Error: " . mysqli_error($mysqlConnection));
+			
+			// select the current database we want, failing if there is an error
+			mysqli_select_db($mysqlConnection, DB_NAME) or die("Error: " . mysqli_error($mysqlConnection));
+			
+			// put the results into the $queryData variable
+			$this->queryData = mysqli_query($mysqlConnection, $newDatabaseQueryString);
+			
+			// disconnect from the database
+			mysqli_close($mysqlConnection);
+		}
+	}
+	
+	// This method is redefined in it's child classes to handle
+	//  database query data accordingly
+	protected function parseDatabaseReturnInfo() {
+		// uses the $queryData attribute that was
+		// populated by queryDatabase to parse the
+		// results accordingly for use by respective WebInterface class
+	}
+	
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,98 @@
+<?php
+/*------------------------------------------------
+// by: Gene Dodge
+//
+//  This script contains all of the HTML for the 
+//    user interface side of things. It serves as
+//    a templated HTML page and calls the proper
+//    php functions based on what the user is doing.
+//
+//  Extends:
+//    NONE
+//
+//  Extended by:
+//    NONE
+//
+//  Requires:
+//   LoginWebInterface.php
+//   PatientWebInterface.php
+//   DoctorWebInterface.php
+//
+//  Required by:
+//    NONE
+//
+//  Attributes:
+//    $_SESSION  // holds session information for use by other scripts
+//
+//  Methods:
+//    callInterface(string)  // calls appropriate interface script
+//                           //  based on what the user is doing
+------------------------------------------------*/
+
+// --------- Session ---------
+// start a session for the user if one does not exist
+//
+//  NOTE: These 3 lines of code need to be at the top of this
+//    php script as well as the following BEFORE any other code:
+//    LoginWebInterface.php
+//    PatientWebInterface.php
+//    DoctorWebInterface.php
+//
+//  The $_SESSION array is used to keep track of who is logged in
+//  within the functionality of login, doctor, or patient
+//  various $_SESSION indices may be set and used.
+//
+//  $_SESSION['user_type'] is checked within the main body
+//  of index.php to determine which of the following functions
+//  to call. (see the if else statement in the body section)
+//
+if (session_status() == PHP_SESSION_NONE) {
+	session_start();
+}
+
+// error reporting REMOVE BEFORE FINAL COMMIT !!!++++=================================================++++!+!+!!!!
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
+?>
+
+<html>
+<head>
+	<title>
+		EWoB : Connecting People to the Care They Deserve
+	</title>
+	<link rel="stylesheet" href="project.css" />
+</head>
+<body>
+	<?php
+		// based on whether or not the user has logged in
+		//  and what type of user it is, do the appropriate
+		//  action (show appropriate html page)
+		
+		if (empty($_SESSION['user_type'])) {
+			// if there is not yet a user, call the login script
+			
+			include 'LoginWebInterface.php';
+		} else {
+			if ($_SESSION['user_type'] == 'DOCTOR') {
+			// if the user has logged in and it is a Doctor,
+			//   call the doctor interface function
+			
+				include 'DoctorWebInterface.php';
+			} else if ($_SESSION['user_type'] == 'PATIENT') {
+			// if the user has logged in and it is a patient,
+			//   call the patient interface function
+			
+				include 'PatientWebInterface.php';
+			} else {
+				// if the session has user_type stored as something else,
+				//   let the user know there was an error and attempt to reset
+				//   by asking them to log out
+				
+				echo "We're sorry, there was an error. Please close this page and try again.<br /> You may need to enable cookies for this site to work.";
+			}
+		}
+	?>
+</body>
+</html>
