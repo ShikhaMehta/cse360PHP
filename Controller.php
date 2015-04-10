51e3f3bbd88b8d1f524b552f6e8b65c14f8ca1ab
Gene Dodge <gdodge@asu.edu>
2015-04-10 11:10:33 -0700

    Updated Controller class database constants to connect to 'patientdb'.

--- Controller.php
+++ Controller.php
@@ -47,16 +47,7 @@ define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
-
-// global variables that can be used for connecting
-// for a specific example of connection see the function
-//   queryDatabase()
-$dbhost = constant("DB_HOST");     // Host name 
-$dbport = constant("DB_PORT");     // Host port
-$dbusername = constant("DB_USER"); // Mysql username 
-$dbpassword = constant("DB_PASS"); // Mysql password 
-$db_name = constant("DB_NAME");    // Database name
+define('DB_NAME', 'patientdb');
 
 Class Controller {
 
@@ -139,7 +130,7 @@ Class Controller {
 			
 			// put the results into the $queryData variable
 			$this->queryData = mysqli_query($mysqlConnection, $this->databaseQueryString);
-echo '"' . $this->databaseQueryString . '"<br />';			
+
 			// disconnect from the database
 			mysqli_close($mysqlConnection);
 		}
