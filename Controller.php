20200e1356cedd59afdb66533ceb1c5013bfdc1a
Gene Dodge <gdodge@asu.edu>
2015-04-10 10:13:54 -0700

    Moved constants inside the class body to fix dependency issues. *hopefully* Commented inherited variables.

--- Controller.php
+++ Controller.php
@@ -40,25 +40,27 @@
 //
 ------------------------------------------------*/
 
-// database setup for doing MySQL queries
-// This redefines constants that can be used to connect
-//   to the MySQL database on Openshift.com
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
-
-// global variables that can be used for connecting
-// for a specific example of connection see the function
-//   queryDatabase()
-$dbhost = constant("DB_HOST");     // Host name 
-$dbport = constant("DB_PORT");     // Host port
-$dbusername = constant("DB_USER"); // Mysql username 
-$dbpassword = constant("DB_PASS"); // Mysql password 
-$db_name = constant("DB_NAME");    // Database name
-
 Class Controller {
+
+	// database setup for doing MySQL queries
+	// This redefines constants that can be used to connect
+	//   to the MySQL database on Openshift.com
+	define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+	define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+	define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+	define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+	define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+
+	// global variables that can be used for connecting
+	// for a specific example of connection see the function
+	//   queryDatabase()
+	$dbhost = constant("DB_HOST");     // Host name 
+	$dbport = constant("DB_PORT");     // Host port
+	$dbusername = constant("DB_USER"); // Mysql username 
+	$dbpassword = constant("DB_PASS"); // Mysql password 
+	$db_name = constant("DB_NAME");    // Database name
+
+	// inherited variables
 	protected $currentUser;
 	protected $databaseQueryString;
 	protected $queryData;
