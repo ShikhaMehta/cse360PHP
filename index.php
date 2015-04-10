4110574437fc1703cf92a06c045b55e8d8fb6e9d
Gene Dodge <gdodge@asu.edu>
2015-04-10 10:47:46 -0700

    More query adjustments in test code.

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ include 'Controller.php';
 
 $newCtrl = new Controller;
 
-$newCtrl->setQueryString('SELECT * FROM Doctor;');
+$newCtrl->setQueryString('SELECT * FROM authentication;');
 
 $newCtrl->queryDatabase();
 
