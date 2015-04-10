68412f3d85c1ca30a9ec431194670b5223a58aac
Gene Dodge <gdodge@asu.edu>
2015-04-10 11:12:07 -0700

    Updated test code in index.php to do final check of database querying using controller class.

--- index.php
+++ index.php
@@ -80,20 +80,8 @@ $newCtrl->setQueryString('SELECT * FROM doctor');
 
 $newCtrl->queryDatabase();
 
-$returnedData = $newCtrl->getQueryData();
+var_dump($newCtrl->getQueryData());
 
-if (mysqli_num_rows($returnedData) > 0) {
-	while ($row = mysqli_fetch_assoc($returnedData)) {
-	    // do stuff with the data from this row
-	    // the array would hold values for each row in
-        //  this format:	
-	    echo $row['DoctorName'] . $row['Pat1Name'] . '<br />';
-	    // where the first row would correspond to:
-	    // "Albert", "Einstein", 1112223333
-	}
-} else {
-	echo 'No results<br />';
-}
 		
 		if (empty($_SESSION['user_type'])) {
 			// if there is not yet a user, call the login script
