d75176f0419f90744ddeb19aca48c5d8955d07b9
ShikhaMehta <smehta11@asu.edu>
2015-04-17 16:22:46 -0700

    tetsing protected

--- LoginController.php
+++ LoginController.php
@@ -5,135 +5,15 @@
 // it directs them to the DoctorWebInterface or if it is a patient it directs the user to SymptomsWebInterface
  
 require 'Controller.php';
-
-// Global Variables
-$username = $_POST["username"];
-$password = $_POST["password"];
-
 class LoginWebInterface extends Controller
 {
-	// publicsetQueryString - sets query string to query database
 	protected function publicsetQueryString($newQueryString) 
 	{
 		return parent::setQueryString($newQueryString);
 	}
-	
-	//parseDatabaseReturnInfo
 	protected function parseDatabaseReturnInfo() 
 	{
-		$result = publicgetQueryData();
-		if (mysqli_num_rows($result) > 0)
-	{
-		while($row = mysqli_fetch_assoc($result)) 
-		{
-			$username = $row["username"];
-			$password = $row["password"];
-			$DoctorOrPatient = $row["DoctorOrPatient"];
-			// add logic here to customize the html displayed here depending on if the person is a doctor or a patient
-		if($DoctorOrPatient == "Doctor")
-		{
-			echo "<h2>Welcome Back $username</h2>";
-			echo "This page is under construction <br>";
-			//$test->testfun();
-			exit;
-		}
-		else if ($DoctorOrPatient == "Patient")
-		{
-			echo "<html>";
-			echo "<title>Patient Symptoms Page</title>";
-			echo "<link rel=\"stylesheet\" href=\"project.css\" />";
-			echo "<body>";
-
-			echo "<h2><CENTER> Welcome $username.  Please enter your symptoms on a scale of 1 to 10 with 10 being most severe.<br></h2>"; 
-			echo "<br><br>";
-			echo "<form action=\"patient.php\" method=\"post\">";
-			echo " <h2>Are you experiencing any pain?</h2>";
-			echo "<input type=\"radio\" name=\"pain\" value=\"pain_1\"> 1 ";
-			echo "<input type=\"radio\" name=\"pain\" value=\"pain_2\"> 2 ";
-			echo "<input type=\"radio\" name=\"pain\" value=\"pain_3\"> 3 ";
-			echo "<input type=\"radio\" name=\"pain\" value=\"pain_4\"> 4 ";
-			echo "<input type=\"radio\" name=\"pain\" value=\"pain_5\"> 5 ";
-			echo "<input type=\"radio\" name=\"pain\" value=\"pain_6\"> 6 ";
-			echo "<input type=\"radio\" name=\"pain\" value=\"pain_7\"> 7 ";
-			echo "<input type=\"radio\" name=\"pain\" value=\"pain_8\"> 8 ";
-			echo "<input type=\"radio\" name=\"pain\" value=\"pain_9\"> 9 ";
-			echo "<input type=\"radio\" name=\"pain\" value=\"pain_10\"> 10 ";
-			
-			echo "<br><br>";
-			echo " <h2>Are you drowsy?</h2>";
-			echo "<input type=\"radio\" name=\"drowsy\" value=\"drowsy_1\"> 1 ";
-			echo "<input type=\"radio\" name=\"drowsy\" value=\"drowsy_2\"> 2 ";
-			echo "<input type=\"radio\" name=\"drowsy\" value=\"drowsy_3\"> 3 ";
-			echo "<input type=\"radio\" name=\"drowsy\" value=\"drowsy_4\"> 4 ";
-			echo "<input type=\"radio\" name=\"drowsy\" value=\"drowsy_5\"> 5 ";
-			echo "<input type=\"radio\" name=\"drowsy\" value=\"drowsy_6\"> 6 ";
-			echo "<input type=\"radio\" name=\"drowsy\" value=\"drowsy_7\"> 7 ";
-			echo "<input type=\"radio\" name=\"drowsy\" value=\"drowsy_8\"> 8 ";
-			echo "<input type=\"radio\" name=\"drowsy\" value=\"drowsy_9\"> 9 ";
-			echo "<input type=\"radio\" name=\"drowsy\" value=\"drowsy_10\"> 10 ";
-			
-			echo "<br><br>";
-			echo " <h2>Do you have feelings of Nausea?</h2>";
-			echo "<input type=\"radio\" name=\"nausea\" value=\"nausea_1\"> 1 ";
-			echo "<input type=\"radio\" name=\"nausea\" value=\"nausea_2\"> 2 ";
-			echo "<input type=\"radio\" name=\"nausea\" value=\"nausea_3\"> 3 ";
-			echo "<input type=\"radio\" name=\"nausea\" value=\"nausea_4\"> 4 ";
-			echo "<input type=\"radio\" name=\"nausea\" value=\"nausea_5\"> 5 ";
-			echo "<input type=\"radio\" name=\"nausea\" value=\"nausea_6\"> 6 ";
-			echo "<input type=\"radio\" name=\"nausea\" value=\"nausea_7\"> 7 ";
-			echo "<input type=\"radio\" name=\"nausea\" value=\"nausea_8\"> 8 ";
-			echo "<input type=\"radio\" name=\"nausea\" value=\"nausea_9\"> 9 ";
-			echo "<input type=\"radio\" name=\"nausea\" value=\"nausea_10\"> 10 ";
-			
-			echo "<br><br>";
-			echo " <h2>Do you feel anxious?</h2>";
-			echo "<input type=\"radio\" name=\"anxious\" value=\"anxious_1\"> 1 ";
-			echo "<input type=\"radio\" name=\"anxious\" value=\"anxious_2\"> 2 ";
-			echo "<input type=\"radio\" name=\"anxious\" value=\"anxious_3\"> 3 ";
-			echo "<input type=\"radio\" name=\"anxious\" value=\"anxious_4\"> 4 ";
-			echo "<input type=\"radio\" name=\"anxious\" value=\"anxious_5\"> 5 ";
-			echo "<input type=\"radio\" name=\"anxious\" value=\"anxious_6\"> 6 ";
-			echo "<input type=\"radio\" name=\"anxious\" value=\"anxious_7\"> 7 ";
-			echo "<input type=\"radio\" name=\"anxious\" value=\"anxious_8\"> 8 ";
-			echo "<input type=\"radio\" name=\"anxious\" value=\"anxious_9\"> 9 ";
-			echo "<input type=\"radio\" name=\"anxious\" value=\"anxious_10\"> 10 ";
-			
-			echo "<br><br>";
-			echo " <h2>Do you feel depressed?</h2>";
-			echo "<input type=\"radio\" name=\"depressed\" value=\"depressed_1\"> 1 ";
-			echo "<input type=\"radio\" name=\"depressed\" value=\"depressed_2\"> 2 ";
-			echo "<input type=\"radio\" name=\"depressed\" value=\"depressed_3\"> 3 ";
-			echo "<input type=\"radio\" name=\"depressed\" value=\"depressed_4\"> 4 ";
-			echo "<input type=\"radio\" name=\"depressed\" value=\"depressed_5\"> 5 ";
-			echo "<input type=\"radio\" name=\"depressed\" value=\"depressed_6\"> 6 ";
-			echo "<input type=\"radio\" name=\"depressed\" value=\"depressed_7\"> 7 ";
-			echo "<input type=\"radio\" name=\"depressed\" value=\"depressed_8\"> 8 ";
-			echo "<input type=\"radio\" name=\"depressed\" value=\"depressed_9\"> 9 ";
-			echo "<input type=\"radio\" name=\"depressed\" value=\"depressed_10\"> 10 ";
-			
-			echo "<br><br>";
-			echo "<input type=\"submit\"></CENTER>";
-			echo "</form>";
-			echo "</body>";
-			echo "</html>";
-
-
-			//echo "<h2>Welcome Back $username</h2>";
-			//echo " Shikha is going to work on this page shortly <br>";
-			exit;
-		}
-		else
-		{
-			echo "$username $password $DoctorOrPatient<br>";
-			echo "there seems to be an error <br>. Please check entry in the database";
-			exit;
-		}
-        //echo "<h2>Welcome Back $username</h2>";
-       // echo "$username $password $DoctorOrPatient<br>";
-		exit;
-    }
-}
-header("Location: http://engineers-withoutborders.rhcloud.com/login_error.html");
+		;
 	}
 
 	protected function publicqueryDatabase() 
@@ -147,27 +27,26 @@ header("Location: http://engineers-withoutborders.rhcloud.com/login_error.html")
 	}
 }
 
-//$username = $_POST["username"];
-//$password = $_POST["password"];
+$username = $_POST["username"];
+$password = $_POST["password"];
 $test = new LoginWebInterface();
 
 $test->publicsetQueryString("SELECT * FROM `authentication` WHERE username = '$username' and password = '$password';");
 $test->publicqueryDatabase();
-$test->parseDatabaseReturnInfo();
 
 
 // these variables need to be used since openshift probably has a firewall that doesn't allow
 // external access to their mysql database, access is only through phpmyadmin or ssh to the actual server
-//$OPENSHIFT_MYSQL_DB_HOST = getenv('OPENSHIFT_MYSQL_DB_HOST');
-//$OPENSHIFT_MYSQL_DB_PORT = getenv('OPENSHIFT_MYSQL_DB_PORT');
-//$dbname = "patientdb";
+$OPENSHIFT_MYSQL_DB_HOST = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$OPENSHIFT_MYSQL_DB_PORT = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbname = "patientdb";
 
 // debug
 //echo $OPENSHIFT_MYSQL_DB_HOST . "\n";
 //echo $OPENSHIFT_MYSQL_DB_PORT . "\n";
 
-//$link = mysql_connect("$OPENSHIFT_MYSQL_DB_HOST","adminCjp7HQy","nuL1MDKTyQkl") or die("Error: Cannot connect to database.\n"); 
-//mysql_select_db($dbname);
+$link = mysql_connect("$OPENSHIFT_MYSQL_DB_HOST","adminCjp7HQy","nuL1MDKTyQkl") or die("Error: Cannot connect to database.\n"); 
+mysql_select_db($dbname);
 
 //$username = $_POST["username"];
 //$password = $_POST["password"];
@@ -179,11 +58,11 @@ $test->parseDatabaseReturnInfo();
 //execute the query. 
 
 //$result = mysql_query($query); 
-//$result = $test->publicgetQueryData();
+$result = $test->publicgetQueryData();
 
 //display information: 
 //if ($result) {
-/*if (mysqli_num_rows($result) > 0)
+if (mysqli_num_rows($result) > 0)
 {
     while($row = mysqli_fetch_assoc($result)) {
         $username = $row["username"];
@@ -293,5 +172,5 @@ $test->parseDatabaseReturnInfo();
 		exit;
     }
 }
-header("Location: http://engineers-withoutborders.rhcloud.com/login_error.html");*/
+header("Location: http://engineers-withoutborders.rhcloud.com/login_error.html");
 ?> 
