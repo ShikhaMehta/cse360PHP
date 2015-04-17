a03dc3ec5d4c8e5b74e9aa527366c342a01d2fb5
abnunez <adam.b.nunez@gmail.com>
2015-04-16 19:15:36 -0700

    no message

--- DoctorController.php
+++ DoctorController.php
@@ -50,7 +50,8 @@ Class DoctorController extends Controller
 	public function listOfDetails($PatientName)
 	{
 		//Setting the query string to all symptoms for the input patientName
-		$this->setQueryString("SELECT Symptom1,Symptom2,Symptom3,Symptom4,Symptom5 FROM patient WHERE PatientName is '$PatientName'");
+		//$this->setQueryString("SELECT Symptom1,Symptom2,Symptom3,Symptom4,Symptom5 FROM patient WHERE PatientName is '$PatientName'");
+		$this->setQueryString("SELECT * FROM patient WHERE PatientName is '$PatientName'");
 		echo $this->getQueryString();
 		$this->queryDatabase();
 		
