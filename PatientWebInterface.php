a985f1471ac02340ae9228a8797d4e842448e4f3
ShikhaMehta <smehta11@asu.edu>
2015-04-18 08:48:50 -0700

    demo for gene

--- PatientWebInterface.php
+++ PatientWebInterface.php
@@ -9,11 +9,13 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
+$username = $_SESSION['current_user'];
+
 echo "<html>";
 			echo "<title>Patient Symptoms Page</title>";
 			echo "<body>";
 
-			echo "<h2><CENTER> Welcome $_SESSION['current_user'].  Please enter your symptoms on a scale of 1 to 10 with 10 being most severe.<br></h2>"; 
+			echo "<h2><CENTER> Welcome $username.  Please enter your symptoms on a scale of 1 to 10 with 10 being most severe.<br></h2>"; 
 			echo "<br><br>";
 			echo "<form action=\"patient.php\" method=\"post\">";
 			echo " <h2>Are you experiencing any pain?</h2>";
