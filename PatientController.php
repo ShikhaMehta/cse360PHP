ae6ec148aab1e9de0261623ff52914adaabb6c5b
Alisha Geis <angeis@asu.edu>
2015-04-20 12:30:32 -0700

    Added required Controller.php, extended class to controller and included the parent constructor.

--- PatientController.php
+++ PatientController.php
@@ -1,2 +1,27 @@
 <?php
+
+/*------------------------------------------------
+// by : Alisha Geis
+//
+//  Extends:
+//  Controller. 
+//
+//  Extended by:
+//  None.
+//
+//  Requires:
+//  NONE
+//
+//  Required by:
+//  NONE
+//
+//  Attributes:
+//    $_SESSION  // holds session information for use by other scripts
+------------------------------------------------*/
+require 'Controller.php';
+Class PatientController extends Controller
+	function __construct() 
+	{
+		parent::__construct();
+	}
 ?>
