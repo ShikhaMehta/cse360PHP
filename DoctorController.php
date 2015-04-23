89d66cd531e5a09e376c94ef733b986f7913f868
Oluwatosin Ajayi <ojajayi@asu.edu>
2015-04-22 17:51:13 -0700

    debugging doctor controller.

--- DoctorController.php
+++ DoctorController.php
@@ -59,13 +59,15 @@ Class DoctorController extends Controller
 					{
 						//var_dump($this->getQueryData());
 						$symptomresults = mysqli_fetch_assoc($this->getQueryData());
-						$currentpatientindex = 'patient' . $i; 
+						var_dump ($symptomresults);
+						/*$currentpatientindex = 'patient' . $i; 
 						$patients[$currentpatientindex . 'name'] = $doctorresults["$currentpatient"];
 						$patients[$currentpatientindex . 'mean'] = $this->calculatemean($symptomresults['Symptom1'],$symptomresults['Symptom2'],$symptomresults['Symptom3'],$symptomresults['Symptom4'],$symptomresults['Symptom5']);	
 						echo $patients[$currentpatientindex . 'name'];
 						echo "<br/>";
 						echo $patients[$currentpatientindex . 'mean'];
-						echo "<br/>";
+						echo "<br/>";*/
+						
 					}
 				}				
 			}
